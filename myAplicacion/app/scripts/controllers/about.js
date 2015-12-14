b55ac7fefae0ace967058656667b2a75a40c5162
arturo gonzalez <arturo@agcapa.com>
2015-12-14 14:17:37 +0100

    angular

---
+++ myAplicacion/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAplicacionApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the myAplicacionApp
+ */
+angular.module('myAplicacionApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ myAplicacion/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAplicacionApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myAplicacionApp
+ */
+angular.module('myAplicacionApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
